f4b5a52f48c526cbce4e5fd0a1fc99fdc0bbf564
Karthikeyan N <109551096+Karthikoffl@users.noreply.github.com>
2023-04-04 23:04:21 +0530

    initial commit

---
+++ lib/controller/global_controller.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+
+class GlobalController extends GetxController{
+  //create various variables
+  final RxBool _isLoading = true.obs;
+  final RxDouble _lattitude = 0.0.obs;
+  final RxDouble _longitude = 0.0.obs;
+
+
+  //instance for them to be called
+  RxBool checkLoading() => _isLoading;
+  RxDouble getLattitude() => _lattitude;
+  RxDouble getLongitude() => _longitude;
+
+
+  @override
+  void onInit() {
+    if (_isLoading.isTrue){
+      getLocation();
+    }
+    super.onInit();
+  }
+
+
+  getLocation() async { 
+
+  }
+}
